bcf1cf064faadfe5f2aca5e46e967bb6a0058f0f
Mr. Clusterman <mrclusterman@gmail.com>
2024-12-21 17:58:25 +0100

    Fix images Error

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,7 @@ export const projects: Project[] = [
 		description:
 			"This Node.js project uses Axios and Cheerio to scrape IPTV channel links from a webpage and generate an M3U playlist. It extracts and filters valid stream URLs, creating a custom IPTV playlist with channel names and links for easy streaming.",
 		technologies: ["JavaScript", "NodeJS", "Web Scraping Utilities"],
-		imageUrl: "/images/projects/nodejs-logo.svg",
+		imageUrl: "./images/projects/nodejs-logo.svg",
 		projectUrl: "https://github.com/Mrclusterman/IPTV_Indexer",
 		githubUrl: "https://github.com/Mrclusterman/IPTV_Indexer",
 	},
@@ -23,7 +23,7 @@ export const projects: Project[] = [
 			"TypeScript",
 			"Web Scraping Utilities",
 		],
-		imageUrl: "/images/projects/react-logo.svg",
+		imageUrl: "./images/projects/react-logo.svg",
 		projectUrl: "https://mrclusterman.github.io/m3u-scrapper-web/",
 		githubUrl:
 			"https://github.com/Mrclusterman/m3u-scrapper-web/tree/master",
@@ -34,7 +34,7 @@ export const projects: Project[] = [
 		description:
 			"A modern portfolio website built to showcase my projects and skills. Features a responsive design, dark mode support, and interactive project cards with detailed information about each project.",
 		technologies: ["React", "Next.js", "Tailwind CSS", "TypeScript"],
-		imageUrl: "/images/projects/nextjs-logo.svg",
+		imageUrl: "./images/projects/nextjs-logo.svg",
 		projectUrl: "", // Add your portfolio URL here
 		githubUrl: "", // Add your portfolio's GitHub URL here
 	},
